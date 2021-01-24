29f4fc1cdf054156223a4baa52f08c7fbe949afe
kamel.khbr@gmail.com <!055686Moii>
2021-01-23 19:46:27 -0500

    tool/ search bar added

--- app/src/main/java/org/mousehole/restolocatorkameljohn/view/MainActivity.kt
+++ app/src/main/java/org/mousehole/restolocatorkameljohn/view/MainActivity.kt
@@ -2,11 +2,30 @@ package org.mousehole.restolocatorkameljohn.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 import org.mousehole.restolocatorkameljohn.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Creating the menu file for the main acitvity
+        toolbar = findViewById(R.id.main_tool_bar)
+        setSupportActionBar(toolbar)
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
 }
