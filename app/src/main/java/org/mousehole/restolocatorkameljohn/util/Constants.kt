8da45f111d584b9cccaec1269a198fde183f988a
John Le <johnhle7@gmail.com>
2021-01-24 00:17:07 -0500

    Map animation to current location & adding search radius notes to Constants.kt

--- app/src/main/java/org/mousehole/restolocatorkameljohn/util/Constants.kt
+++ app/src/main/java/org/mousehole/restolocatorkameljohn/util/Constants.kt
@@ -12,5 +12,20 @@ class Constants {
         const val SEARCH_RADIUS= "radius"
         const val MY_API_KEY = "AIzaSyDh8h-16Q-uDkppVJsfPFjqS-U2OxSyfi8"
 
+        const val LOCATION_REQUEST_CODE = 707
+
+
+        const val TAG = "TAG_X"
+
+
+        // Example of searching for nearby places based on current location radius
+        // Must have 3 Parameters
+        //      1) Location -> Long, Lat
+        //      2) Radius -> meters (Max is 50000 meters)
+        //      3) API Key
+        /*https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.9091,-84.4791&radius=1500&key=AIzaSyDh8h-16Q-uDkppVJsfPFjqS-U2OxSyfi8*/
+
+        // Other parameters for querys -> https://developers.google.com/places/web-service/search
+
     }
 }
--- app/src/main/java/org/mousehole/restolocatorkameljohn/view/MapsActivity.kt
+++ app/src/main/java/org/mousehole/restolocatorkameljohn/view/MapsActivity.kt
@@ -2,11 +2,18 @@ package org.mousehole.restolocatorkameljohn.view
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,12 +21,22 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 import org.mousehole.restolocatorkameljohn.R
+import org.mousehole.restolocatorkameljohn.util.Constants
+import org.mousehole.restolocatorkameljohn.util.Constants.Companion.LOCATION_REQUEST_CODE
+import org.mousehole.restolocatorkameljohn.util.Constants.Companion.TAG
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     private lateinit var locationManager: LocationManager
     private lateinit var mMap: GoogleMap
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private  var currentLat: Double = 0.0
+    private  var currentLong: Double = 0.0
+
+    private lateinit var currentLocaitonResetButton: CardView
 
 
 
@@ -30,17 +47,47 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        currentLocaitonResetButton = findViewById(R.id.btn_reset_current)
+        currentLocaitonResetButton.setOnClickListener {
+            moveCameraLocation()
+        }
     }
-
-
 
 
     @SuppressLint("MissingPermission")
     override fun onStart() {
         super.onStart()
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3000L,2f,this)
 
+        requestLocationPermission()
+        registerLocationManager()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(LOCATION_REQUEST_CODE == requestCode){
+            if(permissions[0] == android.Manifest.permission.ACCESS_FINE_LOCATION){
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onRequestPermissionsResult: Location Permission Granted")
+                    registerLocationManager()
+                } else {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission
+                            .ACCESS_FINE_LOCATION)){ // Checks if check box is checked to not ask again
+                        requestLocationPermission()
+                    } else {
+                        // At this point, let the user know that they have to enable permission manually
+                        // to use this application
+                        Log.d(TAG, "onRequestPermissionsResult: Rationale blocked...")
+                    }
+                }
+            }
+        }
     }
 
     override fun onStop() {
@@ -51,15 +98,53 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        Log.d(TAG, "onMapReady: ")
     }
 
     override fun onLocationChanged(location: Location) {
 
-        if (this::mMap.isInitialized) {
-            val currentLocation = LatLng(location.latitude,location.longitude)
-            mMap.addMarker(MarkerOptions().position(currentLocation).title("My Current Location"))
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+        // Gets the current long and lat of the previous location
+        getCurrentLocation()
 
+        Log.d(TAG, "onLocationChanged: getCurrentLocation $currentLat and $currentLong")
+
+
+        moveCameraLocation()
+    }
+
+    private fun moveCameraLocation() {
+        if (this::mMap.isInitialized) {
+            val currentLocation = LatLng(currentLat, currentLong)
+
+            Log.d(TAG, "onLocationChanged: Current Location -> ${currentLocation}")
+            /*mMap.addMarker(MarkerOptions().position(currentLocation).title("My Current Location"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))*/
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
+
+        }
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun registerLocationManager() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10.0f, this)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation(){
+        val task: Task<Location> = fusedLocationProviderClient.lastLocation
+
+        // Getting current lat and current long
+        task.addOnSuccessListener{
+            if(it != null){
+                currentLat = it.latitude
+                currentLong = it.longitude
+            }
         }
     }
 }
