45273c4b2b42de6f9647852af965345d7e0c7fa0
John Le <johnhle7@gmail.com>
2021-01-25 11:13:30 -0500

    Map starts with current position

--- app/src/main/java/org/mousehole/restolocatorkameljohn/view/MainActivity.kt
+++ app/src/main/java/org/mousehole/restolocatorkameljohn/view/MainActivity.kt
@@ -11,8 +11,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar: Toolbar
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
