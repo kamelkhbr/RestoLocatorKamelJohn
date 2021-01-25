99e38482501cb8d8a7b9d6617c68a5221e68e67e
John Le <johnhle7@gmail.com>
2021-01-25 10:40:21 -0500

    Fixed nullable database

--- app/src/main/java/org/mousehole/restolocatorkameljohn/viewmodel/PlacesViewModel.kt
+++ app/src/main/java/org/mousehole/restolocatorkameljohn/viewmodel/PlacesViewModel.kt
@@ -1,11 +1,14 @@
 package org.mousehole.restolocatorkameljohn.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import org.mousehole.restolocatorkameljohn.model.data.LocationPlace
 import org.mousehole.restolocatorkameljohn.model.data.PlacesResult
 import org.mousehole.restolocatorkameljohn.model.db.LocationRepository
+import org.mousehole.restolocatorkameljohn.util.Constants.Companion.TAG
 
 class PlacesViewModel(application: Application) : AndroidViewModel(application){
 
@@ -13,8 +16,11 @@ class PlacesViewModel(application: Application) : AndroidViewModel(application){
 
     val locationLiveData: MutableLiveData<List<PlacesResult>> = repository.placesLiveData
 
+    lateinit var observer: Observer<List<PlacesResult>>
+
     fun getPlaceResultSearchRetro(location: String, radius: String){
         repository.getPlaceResults(location, radius)
+        storeLastLocationResult()
     }
 
     fun insertPlaceDB(location: LocationPlace){
@@ -34,5 +40,46 @@ class PlacesViewModel(application: Application) : AndroidViewModel(application){
     }
 
     fun getPlaceResultSearchDB() = repository.getAllLocationDB()
+
+    fun storeLastLocationResult(){
+        clearDB()
+        observer = Observer {
+            it.forEach {
+
+                val name:String? = it.name
+                val businessStatus:String? = it.business_status
+                val imageIconUrl:String? = it.icon
+                val lat:String? = it.geometry.location.lat.toString()
+                val long:String? = it.geometry.location.lng.toString()
+                var openingHours: String? = null
+                if(it.opening_hours == null){
+                    Log.d(TAG, "storeLastLocationResult: is Null")
+                    openingHours = ""
+                } else {
+                    Log.d(TAG, "storeLastLocationResult: is not null")
+                    openingHours = it.opening_hours.open_now.toString()
+                }
+                val rating:String? = it.rating.toString()
+                insertPlaceDB(
+                    LocationPlace(
+                            name,
+                            businessStatus,
+                            imageIconUrl,
+                            lat,
+                            long,
+                            openingHours,
+                            rating
+
+                    )
+                )
+            }
+        }
+        locationLiveData.observeForever(observer)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        locationLiveData.removeObserver(observer)
+    }
 
 }
